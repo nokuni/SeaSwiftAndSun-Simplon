890a3b38d11a1013e5626428df251ebc86637e59
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-12-11 16:21:51 +0100

    feat: Add of API manager

---
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  SeaSwiftAndSun-Simplon
+//
+//  Created by Yann Christophe Maertens on 11/12/2023.
+//
+
+import Foundation
---
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Extensions/StringExtension.swift
@@ -0,0 +1,8 @@
+//
+//  StringExtension.swift
+//  SeaSwiftAndSun-Simplon
+//
+//  Created by Yann Christophe Maertens on 11/12/2023.
+//
+
+import Foundation
