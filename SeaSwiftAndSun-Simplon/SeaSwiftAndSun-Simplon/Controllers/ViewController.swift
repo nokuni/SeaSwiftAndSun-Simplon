cf65c2eacee05aa88c0ca9f3cf68ae8bd8204bcd
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-12-13 09:29:35 +0100

    feat: Added picker switch and collection view

--- SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/ViewController.swift
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/ViewController.swift
@@ -9,26 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var collectionView: UICollectionView?
+    
     let apiManager = APIManager()
     let url = "https://api.airtable.com/v0/appLwnyGpn1sS3QSc/Surf%20Destinations"
     let token = "patKTpR8A45bdjdCl.e09bf84bc75e3c079b6ceda4de5abe8a2ee4bda1a82c268f98cb4667494b20dd"
     var records = [Record]()
     
+    var currentDisplay: ViewDisplay = .list
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Liste des spots de surf"
+        segmentedControl.addTarget(self, action: #selector(switchDisplay), for: .valueChanged)
+        configureCollection()
+        collectionView?.isHidden = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        DispatchQueue.main.async {
-            Task {
-                let data: RecordData = try await self.apiManager.get(url: self.url, token: self.token)
-                let records = data.records
-                self.records = records
-                self.tableView.reloadData()
-            }
-        }
+//        DispatchQueue.main.async {
+//            Task {
+//                let data: RecordData = try await self.apiManager.get(url: self.url, token: self.token)
+//                let records = data.records
+//                self.records = records
+//                self.tableView.reloadData()
+//            }
+//        }
     }
     
     var fields: [Fields] { records.map(\.fields) }
@@ -89,6 +98,62 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let fields = getSectionFields(section: indexPath.section)
         let field = fields[indexPath.row]
         cell.setUpCell(field: field)
+        return cell
+    }
+}
+
+// MARK: Switch display
+extension ViewController {
+    
+    private func switchToList() {
+        self.tableView.isHidden = false
+        self.collectionView?.isHidden = true
+        currentDisplay = .list
+    }
+    
+    private func switchToCards() {
+        self.tableView.isHidden = true
+        self.collectionView?.isHidden = false
+        currentDisplay = .cards
+    }
+    
+    @objc func switchDisplay() {
+        switch currentDisplay {
+        case .list:
+            switchToCards()
+        case .cards:
+            switchToList()
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func configureCollection() {
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width * 0.9, 
+                                 height: UIScreen.main.bounds.height * 0.3)
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "SpotCard")
+        collectionView?.backgroundColor = UIColor.white
+        view.addSubview(collectionView ?? UICollectionView())
+        
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, 
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SpotCard", for: indexPath)
+        cell.backgroundColor = .black
+        cell.layer.cornerRadius = 20
         return cell
     }
 }
---
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Model/ViewDisplay.swift
@@ -0,0 +1,13 @@
+//
+//  ViewDisplay.swift
+//  SeaSwiftAndSun-Simplon
+//
+//  Created by Yann Christophe Maertens on 12/12/2023.
+//
+
+import Foundation
+
+enum ViewDisplay {
+    case list
+    case cards
+}
